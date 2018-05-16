819b68f4bc072a3cd326c9064d1020bfd3e099c6
Colin Sheppard <colinsheppard10@gmail.com>
2018-05-16 17:16:54 -0400

    added scrolling

--- src/components/modelBasicExample.js
+++ src/components/modelBasicExample.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import ColumnWithPopout from './column_with_popout';
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Grid, Container } from 'semantic-ui-react'
+import Slider from "react-slick";
+
 
 const ModalBasicExample = ({ values, heading, paragraph, image }) => (
     <Modal size='large' trigger={<ColumnWithPopout heading={heading} paragraph={paragraph}></ColumnWithPopout>} >
         <Modal.Header>{heading}</Modal.Header>
-        <Modal.Content image>
-            <Image wrapped fluid src={image} />
+        <Modal.Content image scrolling>
+            <Grid container stackable verticalAlign='middle'>
+                <Grid.Row>
+                    <Image size='huge' wrapped src={image} />
+                </Grid.Row>
+                <Grid.Row>
+                    <Image size='huge' wrapped src={image} />
+                </Grid.Row>
+            </Grid>
             <Modal.Description>
-                <Header>{values}</Header>
-                <p>{paragraph}</p>
+                <Container text className='colcontainer'>
+                    <Header>{values}</Header>
+                    <p>{paragraph} </p>
+                </Container>
             </Modal.Description>
         </Modal.Content>
     </Modal>
 )
 
 export default ModalBasicExample
+
+
+
