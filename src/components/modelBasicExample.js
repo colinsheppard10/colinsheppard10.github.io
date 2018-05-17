d43ce8fbf19afa6b2fccc20b152dabe01d447958
Colin Sheppard <colinsheppard10@gmail.com>
2018-05-17 11:50:03 -0400

    hacked model clipping bottom

--- src/components/modelBasicExample.js
+++ src/components/modelBasicExample.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import ColumnWithPopout from './column_with_popout';
-import { Button, Header, Image, Modal, Grid, Container } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Grid, Container, Icon } from 'semantic-ui-react'
 import Slider from "react-slick";
 
 
 const ModalBasicExample = ({ values, heading, paragraph, image }) => (
-    <Modal size='large' trigger={<ColumnWithPopout heading={heading} paragraph={paragraph}></ColumnWithPopout>} >
-        <Modal.Header>{heading}</Modal.Header>
+    <Modal size='large scrolling' trigger={<ColumnWithPopout heading={heading} paragraph={paragraph}></ColumnWithPopout>} closeIcon>
+        <Modal.Header>
+            {heading}
+        </Modal.Header>
         <Modal.Content image scrolling>
-            <Grid container stackable verticalAlign='middle'>
-                <Grid.Row>
-                    <Image size='huge' wrapped src={image} />
-                </Grid.Row>
-                <Grid.Row>
-                    <Image size='huge' wrapped src={image} />
-                </Grid.Row>
-            </Grid>
+            <Image wrapped fluid src={image} />
             <Modal.Description>
-                <Container text className='colcontainer'>
-                    <Header>{values}</Header>
-                    <p>{paragraph} </p>
-                </Container>
+                <Header>{values}</Header>
+                <p>{paragraph}</p>
             </Modal.Description>
         </Modal.Content>
+        <Modal.Actions>
+            <Button primary>
+                <Icon size='large' name='github' /> GitHub
+            </Button>
+        </Modal.Actions>
     </Modal>
 )
 
 export default ModalBasicExample
+
+
+
 
 
 
