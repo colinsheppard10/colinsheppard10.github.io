ca7b5f7e28de949ad0ddf234361b74d49ada304f
colinsheppard10 <colinsheppard10@gmail.com>
2023-03-17 16:49:34 -0400

    remove twist

--- twist/src/utils/Constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_AWS_URL = 'https://know-your-way.herokuapp.com';
-export const EMAIL_REGEX =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
